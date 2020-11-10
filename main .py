d798a2a1197501a2b19ac5f16caabaad477448d3
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2020-11-10 18:55:14 +0800

    Update main .py

--- main .py
+++ main .py
@@ -1,10 +1,12 @@
 from helpers.load_data import load_data
 from helpers.feature_extraction import feature_extraction
 from helpers.convert_data import convert_data
-
+from helpers.model import cnn
 
 # audiodf = load_data()
 
 featuresdf = feature_extraction()
-
-x_train, x_test, y_train, y_test = convert_data(featuresdf)
+feature = featuresdf.feature
+# print(feature[0].shape)
+#x_train, x_test, y_train, y_test = convert_data(featuresdf)
+# cnn(x_train, x_test, y_train, y_test)
