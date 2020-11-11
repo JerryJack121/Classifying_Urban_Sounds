04368b1d526fa0849a7ad8fdbb9a0ac95c854def
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2020-11-11 13:45:11 +0800

    Update

--- csv2feature.py
+++ csv2feature.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import numpy as np
-from tqdm import tqdm
 import librosa
-import csv
 import pandas as pd
 
 
+labels = pd.read_csv('csv/labels.csv', header = None)
+mfcc = pd.read_csv('csv/mfcc.csv', header = None)
+chromagram = pd.read_csv('csv/chromagram.csv', header = None)
+tonnetz = pd.read_csv('csv/tonnetz.csv', header = None)
+mel = pd.read_csv('csv/mel.csv', header = None)
 
-# labels = pd.read_csv('csv/labels.csv', header = None)
-# mfcc = pd.read_csv('csv/mfcc.csv', header = None)
-# chromagram = pd.read_csv('csv/chromagram.csv', header = None)
-# tonnetz = pd.read_csv('csv/tonnetz.csv', header = None)
-# mel = pd.read_csv('csv/mel.csv', header = None)
 
-feature = pd.read_csv('csv/feature.csv')
--- feature2csv.py
+++ feature2csv.py
@@ -38,21 +38,21 @@ for i in tqdm(range(10)):
     c = np.mean(librosa.feature.chroma_stft(S=s, sr=s_rate).T, axis=0)
     chro.append(c)
 
-# mfcc = pd.DataFrame(mfc)
-# mfcc.to_csv('./csv/mfcc.csv', 'w', index=False, header=False)
-# chromagram = pd.DataFrame(chro)
-# chromagram.to_csv('./csv/chromagram.csv', 'w', index=False, header=False)
-# mel = pd.DataFrame(me)
-# mel.to_csv('./csv/mel.csv', 'w', index=False, header=False)
-# tonnetz = pd.DataFrame(ton)
-# tonnetz.to_csv('./csv/tonnetz.csv', 'w', index=False, header=False)
-# lab_dict = {'classID': lab}
-# la = pd.DataFrame(lab_dict)
-# la.to_csv('./csv/labels.csv', 'w', index=False, header=False)
+mfcc = pd.DataFrame(mfc)
+mfcc.to_csv('./csv/mfcc.csv', index=False, header=False)
+chromagram = pd.DataFrame(chro)
+chromagram.to_csv('./csv/chromagram.csv', index=False, header=False)
+mel = pd.DataFrame(me)
+mel.to_csv('./csv/mel.csv', index=False, header=False)
+tonnetz = pd.DataFrame(ton)
+tonnetz.to_csv('./csv/tonnetz.csv', index=False, header=False)
+lab_dict = {'classID': lab}
+la = pd.DataFrame(lab_dict)
+la.to_csv('./csv/labels.csv', index=False, header=False)
 
 
-feature_dict = {'ID': ID, 'classID': lab, 'mel': me,  'mfcc': mfc,
-                'chromagram': chro, 'tonnetz': ton}
-featuredf = pd.DataFrame(feature_dict)
-featuredf.to_csv('./csv/feature.csv', index=False)
+# feature_dict = {'ID': ID, 'classID': lab, 'mel': me,  'mfcc': mfc,
+#                 'chromagram': chro, 'tonnetz': ton}
+# featuredf = pd.DataFrame(feature_dict)
+# featuredf.to_csv('./csv/feature.csv', index=False)
 print('CSV寫入完成')
