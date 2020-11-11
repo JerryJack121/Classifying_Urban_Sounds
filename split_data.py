7a8825b5e9218247c157000a428f9b47de87588f
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2020-11-11 19:35:00 +0800

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -1,18 +1,24 @@
 from read_csv import read_csv
 from sklearn import preprocessing
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 import numpy as np
 
+def split_data(feature, labels):
+    le = LabelEncoder()
+    le = le.fit(['air_conditioner', ' car_horn', 'children_playing', 'dog_bark',
+                'drilling', 'engine_idling', 'gun_shot', 'jackhammer', 'siren', 'street_music'])
+    # Label encoder
+    labels = np.array(labels)
+    label = le.transform(labels)
 
-feature, labels = read_csv()
-le = LabelEncoder()
-le = le.fit(['air_conditioner', ' car_horn', 'children_playing', 'dog_bark',
-        'drilling', 'engine_idling', 'gun_shot', 'jackhammer', 'siren', 'street_music'])
+    # 正規化
+    stan = StandardScaler()
+    stan = stan.fit(feature)
+    feature = stan.transform(feature)
 
-labels = np.array(labels)
-label = le.transform(labels)
-
-x_train = feature[:5]
-x_test = feature[5:]
-y_train = label[5:]
-y_test
+    x_train = feature[:5]
+    x_test = feature[5:]
+    y_train = label[:5]
+    y_test = label[5:]
+    
+    return x_train,x_test,y_train,y_test
