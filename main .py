f2ab5aba22974e8380f318e83f9a77d27e0ca178
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2020-11-12 21:02:01 +0800

    Update

--- main .py
+++ main .py
@@ -1,8 +1,9 @@
 from read_csv import read_csv
 from split_data import split_data
-from train import training
+from train import training, plot_history
 
 
 feature,labels = read_csv()
 x_train,x_test,y_train,y_test = split_data(feature,labels)
-training(x_train,x_test,y_train,y_test)
+history = training(x_train,x_test,y_train,y_test)
+plot_history(history)
--- train.py
+++ train.py
@@ -25,7 +25,37 @@ def NN_model():
 def training(x_train, x_test, y_train, y_test):
     model = NN_model()
     history = model.fit(x_train, y_train,
-                        batch_size=64, epochs=100,
+                        batch_size=64, epochs=10,
                         validation_data=(x_test, y_test))
 
     model.save('model_1000.h5')
+    return history
+
+def plot_history(history):
+    import matplotlib.pyplot as plt
+
+    history_dict = history.history
+    lose_values = history_dict['loss']
+    val_lose_values = history_dict['val_loss']
+    acc = history_dict['acc']
+    val_acc = history_dict['val_acc']
+
+    epochs = range(1, len(lose_values)+1)
+    #繪製訓練與驗證的損失分數
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(epochs, lose_values,'bo',label='Training loss')
+    plt.plot(epochs, val_lose_values,'b',label='Validation loss')
+    plt.title('Training & Validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    #繪製訓練與驗證的準確度
+    plt.subplot(212)
+    plt.plot(epochs, acc,'bo',label='Training acc')
+    plt.plot(epochs, val_acc,'b',label='Validation acc')
+    plt.title('Training & Validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('accuracy')
+    plt.legend()
+    plt.show()
