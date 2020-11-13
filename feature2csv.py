7e201f64698f4892c92b659869de14eadf599577
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2020-11-13 11:01:01 +0800

    Update

--- feature2csv.py
+++ feature2csv.py
@@ -1,58 +1,63 @@
+# 特徵提取並儲存於csv中
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
 import librosa
 
-data = pd.read_csv('D:/DATASET/UrbanSound8K/train.csv')
-print(data.head())  # To see the dataset
+def feature2csv():
+    data = pd.read_csv('D:/DATASET/UrbanSound8K/train.csv')
+    print(data.head())  # To see the dataset
 
-mfc = []
-lab = []
-ton = []
-me = []
-chro = []
-ID = []
+    mfc = []
+    lab = []
+    ton = []
+    me = []
+    chro = []
+    ID = []
 
-# for i in tqdm(range(len(data))):
-for i in tqdm(range(len(data))):
-    f_name = 'D:/DATASET/UrbanSound8K/Train/'+str(data.ID[i])+'.wav'
-    ID.append(data.ID[i])
-    X, s_rate = librosa.load(f_name, res_type='kaiser_fast')
-    # mfccs
-    mf = np.mean(librosa.feature.mfcc(y=X, sr=s_rate).T, axis=0)
-    mfc.append(mf)
-    l = data.Class[i]
-    lab.append(l)
-    # tonnetz
-    try:
-        t = np.mean(librosa.feature.tonnetz(
-            y=librosa.effects.harmonic(X), sr=s_rate).T, axis=0)
-        ton.append(t)
-    except:
-        print(f_name)
-    # mel-scaled spectrogram
-    m = np.mean(librosa.feature.melspectrogram(X, sr=s_rate).T, axis=0)
-    me.append(m)
-    # chromagram
-    s = np.abs(librosa.stft(X))
-    c = np.mean(librosa.feature.chroma_stft(S=s, sr=s_rate).T, axis=0)
-    chro.append(c)
+    # for i in tqdm(range(len(data))):
+    for i in tqdm(range(len(data))):
+        f_name = 'D:/DATASET/UrbanSound8K/Train/'+str(data.ID[i])+'.wav'
+        ID.append(data.ID[i])
+        X, s_rate = librosa.load(f_name, res_type='kaiser_fast')
+        # mfccs
+        mf = np.mean(librosa.feature.mfcc(y=X, sr=s_rate).T, axis=0)
+        mfc.append(mf)
+        l = data.Class[i]
+        lab.append(l)
+        # tonnetz
+        try:
+            t = np.mean(librosa.feature.tonnetz(
+                y=librosa.effects.harmonic(X), sr=s_rate).T, axis=0)
+            ton.append(t)
+        except:
+            print(f_name)
+        # mel-scaled spectrogram
+        m = np.mean(librosa.feature.melspectrogram(X, sr=s_rate).T, axis=0)
+        me.append(m)
+        # chromagram
+        s = np.abs(librosa.stft(X))
+        c = np.mean(librosa.feature.chroma_stft(S=s, sr=s_rate).T, axis=0)
+        chro.append(c)
 
-mfcc = pd.DataFrame(mfc)
-mfcc.to_csv('./csv/mfcc.csv', index=False, header=False)
-chromagram = pd.DataFrame(chro)
-chromagram.to_csv('./csv/chromagram.csv', index=False, header=False)
-mel = pd.DataFrame(me)
-mel.to_csv('./csv/mel.csv', index=False, header=False)
-tonnetz = pd.DataFrame(ton)
-tonnetz.to_csv('./csv/tonnetz.csv', index=False, header=False)
-lab_dict = {'classID': lab}
-la = pd.DataFrame(lab_dict)
-la.to_csv('./csv/labels.csv', index=False, header=False)
+    mfcc = pd.DataFrame(mfc)
+    mfcc.to_csv('./csv/mfcc.csv', index=False, header=False)
+    chromagram = pd.DataFrame(chro)
+    chromagram.to_csv('./csv/chromagram.csv', index=False, header=False)
+    mel = pd.DataFrame(me)
+    mel.to_csv('./csv/mel.csv', index=False, header=False)
+    tonnetz = pd.DataFrame(ton)
+    tonnetz.to_csv('./csv/tonnetz.csv', index=False, header=False)
+    lab_dict = {'classID': lab}
+    la = pd.DataFrame(lab_dict)
+    la.to_csv('./csv/labels.csv', index=False, header=False)
 
 
-# feature_dict = {'ID': ID, 'classID': lab, 'mel': me,  'mfcc': mfc,
-#                 'chromagram': chro, 'tonnetz': ton}
-# featuredf = pd.DataFrame(feature_dict)
-# featuredf.to_csv('./csv/feature.csv', index=False)
-print('CSV寫入完成')
+    # feature_dict = {'ID': ID, 'classID': lab, 'mel': me,  'mfcc': mfc,
+    #                 'chromagram': chro, 'tonnetz': ton}
+    # featuredf = pd.DataFrame(feature_dict)
+    # featuredf.to_csv('./csv/feature.csv', index=False)
+    print('CSV寫入完成')
+
+if __name__ == "__main__":
+    feature2csv()
--- main .py
+++ main .py
@@ -1,8 +1,13 @@
+from feature2csv import feature2csv
 from read_csv import read_csv
 from split_data import split_data
 from NN import training, model_test
 
 
+# feature2csv()
+#讀取儲存在csv中的data
 feature, labels = read_csv()
+#預處理
 x_train, y_train, x_val, y_val, y_test, y_test = split_data(feature, labels)
+#訓練模型
 training(x_train, x_val, y_train, y_val)
